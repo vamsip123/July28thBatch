fb3e5cb53edd22bb8252cb06ea1ff4729f9a2bd0
Jayram <jayram.h2kinfosys@gmail.com>
2014-08-06 21:34:59 -0400

    Update

--- CoreJava/src/HelloWorld.java
+++ CoreJava/src/HelloWorld.java
@@ -1,4 +1,21 @@
 
 public class HelloWorld {
-
+	//method
+	//api
+	//function
+	
+	public static void main(String[] args) { //Signature
+		int age = 128;
+		
+		float marks = 78.5f;
+		
+		String name = "Jayram";
+		System.out.println("Hello World  " +        age);
+		// ERROR & EXCEPTION
+	}
+	
+	//public : so that this method could be accessed
+	//static : so that this metod could be directly accessed
+	// void : no return type
+	
 }
