2741b7bf6887052ae31827c880b6ebb7e9135a58
Jayram <jayram.h2kinfosys@gmail.com>
2014-08-20 22:38:36 -0400

    Update

---
+++ CoreJava/src/com/h2kinfosys/tutorial/assignments/SwapNumbers.java
@@ -0,0 +1,21 @@
+package com.h2kinfosys.tutorial.assignments;
+
+/**
+ * Java program swaps two numbers using a temporary variable.
+ * @author Jayram
+ * IMPORTANT : Using Google is Prohabited...
+ */
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		System.out.println( "x = "+ x +" y = " + y);
+		//Now Swap the numbers so that when u print the below line x shud be 20
+		//and y shud be 10.
+		// HINT : use a temporary variable
+		// Do the same program without using a temporary variable
+		System.out.println( "x = "+ x +" y = " + y);
+	}
+
+}
