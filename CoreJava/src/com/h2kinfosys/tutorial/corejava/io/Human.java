60ef82ee5d4d55dbea345c4f6d57dd53a167037a
Jayram <jayram.h2kinfosys@gmail.com>
2014-09-10 22:09:44 -0400

    Update

---
+++ CoreJava/src/com/h2kinfosys/tutorial/corejava/io/Human.java
@@ -0,0 +1,7 @@
+package com.h2kinfosys.tutorial.corejava.io;
+
+import java.io.Serializable;
+
+public class Human implements Serializable {
+
+}
--- CoreJava/src/com/h2kinfosys/tutorial/corejava/io/Person.java
+++ CoreJava/src/com/h2kinfosys/tutorial/corejava/io/Person.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
  * @author Jayram
  *
  */
-public class Person implements Serializable {
+public class Person extends Human {
 	
 //	private static final long serialVersionUID = 1395426454629109273L;
 	
